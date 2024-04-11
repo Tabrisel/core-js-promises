0fe2127fbafe9874867cbb59a48ac7fc7446ab19
Elizaveta Zaharova <LizaZaharova99@mail.ru>
2024-04-11 13:03:48 +0300

    feat: method queuPromises complete

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -138,8 +138,12 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  let result = Promise.resolve('');
+  promises.forEach((promise) => {
+    result = result.then((acc) => promise.then((value) => acc + value));
+  });
+  return result;
 }
 
 module.exports = {
